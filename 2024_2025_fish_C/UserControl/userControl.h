f0b834c6482e9abc068ecc4a06017d7d57703072
Charlie Cheung <108029843+Charungg@users.noreply.github.com>
2024-12-16 18:20:56 +0000

    Delete 2024_2025_fish_C directory

--- 2024_2025_fish_C/UserControl/userControl.h
+++
@@ -1,6 +0,0 @@
-#ifndef USERCONTROL_H
-#define USERCONTROL_H
-int isLongPressed(const char* result, int* AFKTimer);
-
-int isShortPressed(const char* result, int* AFKTimer);
-#endif // USERCONTROL_H
